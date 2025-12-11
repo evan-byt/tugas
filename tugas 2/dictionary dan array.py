a30d4584e443dda832f782f9669f77d925855c16
evan-byt <jackyuu2005@gmail.com>
2025-12-11 19:30:04 +0800

    Add files via upload

---
+++ tugas 2/dictionary dan array.py
@@ -0,0 +1,15 @@
+motor = {
+    "merek": "Yamaha",
+    "tipe": "Nmax",
+    "tahun": 2022
+}
+
+print("Merek:", motor["merek"])
+print("Tipe :", motor["tipe"])
+print("Tahun:", motor["tahun"])
+
+angka = [10, 20, 30, 40]
+
+print("Isi array:", angka)
+print("Elemen pertama:", angka[0])
+print("Elemen terakhir:", angka[-1])
